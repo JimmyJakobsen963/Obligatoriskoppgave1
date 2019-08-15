866e8b0a05d9c770e822f99afc4eaebb2c61840e
JimmyJakobsen963 <47241811+JimmyJakobsen963@users.noreply.github.com>
2019-08-15 10:56:39 +0200

    Add files via upload

--- Person.cs
+++ Person.cs
@@ -10,5 +10,10 @@ namespace Obligatorisk_oppgave1
 		public string LastName;
 		public Person Mother;
 		public Person Father;
+
+		public void Show()
+		{
+			Console.WriteLine("| Id: " + Id + " | First Name: " + FirstName + " | Last Name: " + LastName + " | Birth: " + BirthYear + " | Father: " + Father.FirstName + " | Mother: " + Mother.FirstName + " |");
+		}
 	}
 }
